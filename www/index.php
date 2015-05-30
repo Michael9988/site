1c186d56d00939acceae51f9609a6b1f298dde9d
Михаил <felix9988@yandex.ru>
2015-05-30 12:51:20 +0300

    third commit

--- www/index.php
+++ www/index.php
@@ -8,6 +8,7 @@ require_once $dir_lib . "url_class.php";
 $url = new URL();
 $view = $url->getView(); //vitoschit ssilku
 
+//obrashenie k classu
 $class = $view . "Content";
 if (file_exists($dir_lib . $class . "_class.php")) {
     require_once $dir_lib . $class . "_class.php";
